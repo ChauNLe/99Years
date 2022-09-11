7a09e945ad915ac5eea5ae8760958c635421ac5c
Richard Zhou <richardzhou163@gmail.com>
2022-09-11 00:24:42 -0400

    Simple game done

--- public/js/script.js
+++ public/js/script.js
@@ -20,11 +20,13 @@ class App{
         this.score = [5, 5, 5, 5];
         this.isContinue = true;
         this.user_score = 0;
-        this.detail = ["environment", "quality-of-life", "health", "economy"];
+        this.detail = ["environment", "health",  "quality-of-life", "economy"];
         this.environmentScore = document.getElementById("environment-score");
         this.qualityScore = document.getElementById("quality-score");
         this.economyScore = document.getElementById("economy-score");
-        this.healthScore = document.getElementById("health-score")
+        this.healthScore = document.getElementById("health-score");
+        this.endingPage = document.getElementById("ending-page");
+        this.endingResult = document.getElementById("results");
     }
 
     getTitle() {
@@ -35,7 +37,7 @@ class App{
         // code to initialize the app goes here
         console.log("Initializing Home Page App");
 
-        // on click handler for continue 
+        // on click handler for "continue" 
         $("#continue").on("click", (evt) => {
             evt.preventDefault();
             this.questionCard.classList.remove('d-none');
@@ -44,25 +46,38 @@ class App{
             this.updateScores();
         })
 
-        // on click handler for yes button
+        // on click handler for "yes" button
         $("#yes-button").on("click", (evt) => {
             evt.preventDefault();
             this.answerYes(question_bank[this.index]);
         })
 
-        // on click handler for no button
+        // on click handler for "no" button
         $("#no-button").on("click", (evt) => {
             evt.preventDefault();
             this.answerNo(question_bank[this.index]);
         })
 
+        // on click handler for "play again" 
+        $("#play-again").on("click", (evt) => {
+            evt.preventDefault();
+            this.resetScore();
+            this.updateScores();
+            this.endingPage.classList.add('d-none');
+            this.gameIntro.classList.remove('d-none');
+        })
+
+    }
+
+    resetScore() {
+        this.score = [5, 5, 5, 5];
     }
 
     updateScores() {
         this.environmentScore.innerText = this.score[0];
-        this.qualityScore.innerText = this.score[1];
-        this.economyScore.innerText = this.score[2];
-        this.healthScore.innerText = this.score[3];
+        this.qualityScore.innerText = this.score[2];
+        this.economyScore.innerText = this.score[3];
+        this.healthScore.innerText = this.score[1];
     }
 
     displayQuestions() {
@@ -76,7 +91,10 @@ class App{
             this.score[i]+=yes_data[question][i];
             console.log(this.score);
             if (this.score[i]<=0) {
-                alert(this.detail[i]+" factor is too low. Your beloved Earth is dying. Your overall score is "+this.user_score);
+                //alert(this.detail[i]+" factor is too low. Your beloved Earth is dying. Your overall score is "+ this.user_score);
+                this.endingPage.classList.remove('d-none');
+                this.questionCard.classList.add('d-none');
+                this.endingResult.innerText = (this.detail[i]+" factor is too low. Your beloved Earth is dying. Your overall score is "+ this.user_score);
                 this.isContinue = false;
                 break;
             }
@@ -94,7 +112,10 @@ class App{
             this.score[i]+=no_data[question][i];
             console.log(this.score);
             if (this.score[i]<=0) {
-                alert(this.detail[i]+" factor is too low. Your beloved Earth is dying. Your overall score is "+this.user_score);
+                //alert(this.detail[i]+" factor is too low. Your beloved Earth is dying. Your overall score is "+ this.user_score);
+                this.endingPage.classList.remove('d-none');
+                this.questionCard.classList.add('d-none');
+                this.endingResult.innerText = (this.detail[i]+" factor is too low. Your beloved Earth is dying. Your overall score is "+ this.user_score);                this.isContinue = false;
                 this.isContinue = false;
                 break;
             }
