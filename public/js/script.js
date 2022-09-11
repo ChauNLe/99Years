a3cf508a02fef324710766a71f7a70195a5424c6
Richard Zhou <richardzhou163@gmail.com>
2022-09-10 23:33:01 -0400

    got scoreboard working

--- public/js/script.js
+++ public/js/script.js
@@ -15,7 +15,16 @@ class App{
         this.questionCard = document.getElementById("question-page");
         this.continueButton = document.getElementById("continue");
         this.gameIntro = document.getElementById("game-intro");
-        this.question = document.getElementById("questions")
+        this.question = document.getElementById("questions");
+        this.index = 0;
+        this.score = [5, 5, 5, 5];
+        this.isContinue = true;
+        this.user_score = 0;
+        this.detail = ["environment", "quality-of-life", "health", "economy"];
+        this.environmentScore = document.getElementById("environment-score");
+        this.qualityScore = document.getElementById("quality-score");
+        this.economyScore = document.getElementById("economy-score");
+        this.healthScore = document.getElementById("health-score")
     }
 
     getTitle() {
@@ -32,58 +41,68 @@ class App{
             this.questionCard.classList.remove('d-none');
             this.gameIntro.classList.add('d-none');
             this.displayQuestions();
+            this.updateScores();
         })
 
         // on click handler for yes button
         $("#yes-button").on("click", (evt) => {
             evt.preventDefault();
-            this.answerYes();
+            this.answerYes(question_bank[this.index]);
         })
 
         // on click handler for no button
         $("#no-button").on("click", (evt) => {
             evt.preventDefault();
-            this.answerNo();
+            this.answerNo(question_bank[this.index]);
         })
 
     }
 
+    updateScores() {
+        this.environmentScore.innerText = this.score[0];
+        this.qualityScore.innerText = this.score[1];
+        this.economyScore.innerText = this.score[2];
+        this.healthScore.innerText = this.score[3];
+    }
+
     displayQuestions() {
-        let index = ~~(question_bank.length * Math.random());
-        this.question.innerText = question_bank[index];
+        this.index = ~~(question_bank.length * Math.random());
+        this.question.innerText = question_bank[this.index];
     }
 
     answerYes(question){
         let i;
         for (i=0; i<4; i++){
-            score[i]+=yes_data[question][i];
-            document.getElementById(detail[i]).style.width=(score[i]/30);
-            if (score[i]<=0) {
-                alert(detail[i]+" is too low. Your beloved Earth is dying. Your overall score is "+user_score);
-                isContinue = false;
+            this.score[i]+=yes_data[question][i];
+            console.log(this.score);
+            if (this.score[i]<=0) {
+                alert(this.detail[i]+" factor is too low. Your beloved Earth is dying. Your overall score is "+this.user_score);
+                this.isContinue = false;
                 break;
             }
             else {
                 this.displayQuestions();
             }
         }
-        if (isContinue) user_score +=1;
+        this.updateScores();
+        if (this.isContinue) this.user_score +=1;
     }
 
     answerNo(question){
         let i;
         for (i=0; i<4; i++){
-            score[i]+=no_data[question][i];
-            document.getElementById(detail[i]).style.width=(score[i]/30);
-            if (score[i]<=0) {
-                alert(detail[i]+" is too low. Your beloved Earth is dying. Your overall score is "+user_score);
-                isContinue = false;
+            this.score[i]+=no_data[question][i];
+            console.log(this.score);
+            if (this.score[i]<=0) {
+                alert(this.detail[i]+" factor is too low. Your beloved Earth is dying. Your overall score is "+this.user_score);
+                this.isContinue = false;
                 break;
             }
             else {
                 this.displayQuestions();
             }
         }
-        if (isContinue) user_score +=1;
+        this.updateScores();
+        if (this.isContinue) this.user_score +=1;
     }
 }
